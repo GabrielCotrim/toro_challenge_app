17f447b1c9ff32b2e14ba088ddc56560bf095109
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-25 11:47:34 -0300

    Ajusta cores e nome do app

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -4,19 +4,19 @@ import 'package:flutter/cupertino.dart';
 class AppTheme {
   AppTheme._();
 
-  static const String appName = "89 FM Joinville";
+  static const String appName = "Toro Challenge";
 
   static final ThemeData _materialTheme = ThemeData();
   static final CupertinoThemeData _cupertinoTheme = CupertinoThemeData();
 
   static final ThemeData materialTheme = _materialTheme.copyWith(
-    primaryColor: Colors.green.shade800,
+    primaryColor: Colors.purple.shade800,
     colorScheme: _materialTheme.colorScheme.copyWith(
-      secondary: Colors.green.shade600,
+      secondary: Colors.purple.shade600,
     ),
   );
 
   static final CupertinoThemeData cupertinoTheme = _cupertinoTheme.copyWith(
-    primaryColor: Colors.green.shade800,
+    primaryColor: Colors.purple.shade800,
   );
 }
