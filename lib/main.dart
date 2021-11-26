8378b4630d11a853fa82b145ee00040794b863a9
GabrielCotrim <gabrielcotrim@outlook.com>
2021-11-26 20:22:42 -0300

    Adiciona multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app_theme.dart';
 import 'core/widgets/platform_stateless_widget.dart';
+import 'features/presentation/bloc/user_position_bloc.dart';
 import 'features/presentation/pages/home_page.dart';
+import 'injection_container.dart' as di;
+import 'injection_container.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const App());
 }
 
 class App extends PlatformStatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  Widget _buildMultiBlocProvider(Widget child) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<UserPositionBloc>(create: (context) => sl<UserPositionBloc>()),
+      ],
+      child: child,
+    );
+  }
+
   @override
   Widget buildCupertinoWidget(BuildContext context) {
     return CupertinoApp(
       title: AppTheme.appName,
       theme: AppTheme.cupertinoTheme,
-      home: const HomePage(),
+      home: _buildMultiBlocProvider(const HomePage()),
     );
   }
 
@@ -27,7 +41,7 @@ class App extends PlatformStatelessWidget {
     return MaterialApp(
       title: AppTheme.appName,
       theme: AppTheme.materialTheme,
-      home: const HomePage(),
+      home: _buildMultiBlocProvider(const HomePage()),
     );
   }
 }
