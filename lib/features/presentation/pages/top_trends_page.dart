e2def7fea76513db0cda36b93f27ce9d7bba08d8
GabrielCotrim <gabrielcotrim@oultlook.com>
2021-11-27 12:25:02 -0300

    Adiciona bloc build dos top trends

--- lib/features/presentation/pages/top_trends_page.dart
+++ lib/features/presentation/pages/top_trends_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:toro_challenge/features/domain/entities/stock.dart';
+import 'package:toro_challenge/features/presentation/bloc/generic/response_bloc.dart';
+import 'package:toro_challenge/features/presentation/bloc/top_trends_bloc.dart';
+import 'package:toro_challenge/features/presentation/widgets/loading_widget.dart';
+import 'package:toro_challenge/features/presentation/widgets/message_display.dart';
 
 import '../widgets/refresh_indicator_trends_widget.dart';
 
@@ -29,36 +35,51 @@ class _TopTrendsState extends State<TopTrendsPage> {
     ),
   );
 
-  Future _onRefresh() async {}
+  Future _onRefresh() async {
+    context.read<TopTrendsBLoc>().add(GetTopTrendsEvent());
+  }
+
   void _onPayAction(dynamic action) {}
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20.0,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: _title,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(5.0),
-            child: _subtitle,
-          ),
-          Expanded(
-            child: RefreshIndicatorTrendsWidget(
-              onRefresh: _onRefresh,
-              onPayAction: _onPayAction,
+    return BlocBuilder<TopTrendsBLoc, ResponseState>(
+      builder: (BuildContext context, ResponseState state) {
+        if (state is Loading) {
+          return const LoadingWidget();
+        } else if (state is Loaded<List<Stock>>) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
             ),
-          ),
-        ],
-      ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: _title,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: _subtitle,
+                ),
+                Expanded(
+                  child: RefreshIndicatorTrendsWidget(
+                    onRefresh: _onRefresh,
+                    onPayAction: _onPayAction,
+                    stocks: state.response,
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else if (state is Error) {
+          return MessageDisplay(message: state.message);
+        }
+        return Container();
+      },
     );
   }
 }
